8f0cbc24b6c85be77c7b45547c736d430b7278c2
Lior Gnieslaw <liorg@moveo.co.il>
2024-04-01 10:44:11 +0300

    fix

--- src/routers/restaurants.router.ts
+++ src/routers/restaurants.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as restaurantController from '../controllers/restaurants.contorller';
+import * as restaurantController from '../controllers/restaurants.controller';
 
 const router = express.Router();
 
